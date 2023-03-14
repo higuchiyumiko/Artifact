f21d91c298ae01a8cb609f2e74343569af144575
higuchiyumiko <marguerite.0501.hurihura.23@gmail.com>
2023-03-14 10:41:07 +0900

    correct review.php 6

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,6 +11,6 @@ class CategoryController extends Controller
 {
     public function index(Review $review,items $items)
     {
-         return view('posts/index')->with(['review' => $review->getPaginateByLimit()])->with(['items' => $items->get()]);
+         return view('posts/index')->with(['review' => $review->getByLimit()]);
     }
 }
